24123be66831589bfa6ebac67ac87b751884e220
LinqinZhong <2952868313@qq.com>
2025-03-29 18:17:08 +0800

    feat(imports): Enable imports

--- test/webpack.config.js
+++ test/webpack.config.js
@@ -1,30 +1,50 @@
-const path = require('path');
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-    entry: './src/index.af',
-    mode: 'development',
-    resolve: {
-        alias:{
-            '@': path.resolve(__dirname, 'src')
-        }
+  entry: "./src/index.af",
+  mode: "development",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
-    // devtool:"inline-source-map",
-    devServer:{
-        static: 'dist',
-        port: 6644
-    },
-    module: {
-        rules: [
-            {
-                test: /\.af$/,
-                use: '../afast/loader/afast-loader'
-            }
-        ]
-    },
-    plugins:[
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        })
-    ]
-}
+  },
+  devtool: "source-map",
+  devServer: {
+    static: "dist",
+    port: 6644,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 2,
+              sourceMap: true,
+              esModule: false
+            },
+          },
+          {
+            loader: "sass-loader"
+          },
+          "postcss-loader",
+        ],
+        sideEffects: true,
+      },
+      {
+        test: /\.af$/,
+        use: "../afast/loader/afast-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
