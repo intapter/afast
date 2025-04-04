fb925ceda2f14e7a420d02b933ebee662843beb2
LinqinZhong <2952868313@qq.com>
2025-04-04 16:48:13 +0800

    fix

--- packages/afast-loader-react/src/parser/parseObject.js
+++ packages/afast-loader-react/src/parser/parseObject.js
@@ -1,22 +1,20 @@
 const parseValue = require("./parseValue");
 
 const parseObject = (props, noParseKeys, imports) => {
-    if (!props) return undefined;
-    return (
-        "{" +
-        Object.keys(props)
-            .map((key) => {
-                const value =
-                    typeof props[key] === "object"
-                        ? parseObject(props[key], noParseKeys, imports)
-                        : noParseKeys && noParseKeys.includes(key)
-                            ? props[key] // Do not parse this field with function parseValue
-                            : parseValue(props[key], imports);
-                return `${key}: ${value}`;
-            })
-            .join(",") +
-        "}"
-    );
+  if (!props) return undefined;
+  return (
+    "{" +
+    Object.keys(props)
+      .map((key) => {
+        const value =
+          noParseKeys && noParseKeys.includes(key)
+            ? props[key] // Do not parse this field with function parseValue
+            : parseValue(props[key], imports);
+        return `${key}: ${value}`;
+      })
+      .join(",") +
+    "}"
+  );
 };
 
 module.exports = parseObject;
